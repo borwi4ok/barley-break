9f5a7241021517d2e6331e73bc432b965b3aca15
borwi4ok <donnos98@gmail.com>
2021-02-24 20:41:42 +0300

    fully ready game

--- src/components/Game/Game.vue
+++ src/components/Game/Game.vue
@@ -13,8 +13,7 @@
 					<p>{{el.value}}</p>
 				</div>
 			</template>
-
-			<button v-if="isWin" class="isWinBtn">Shuffle</button>
+			<button v-if="isWin" class="isWinBtn" v-on:click="shuffle">Shuffle</button>
 	</div>
 </template>
 
@@ -26,17 +25,21 @@ export default {
 	name:'Game',
 	data(){
 		return{
-			isWin: true,
+			isWin: false,
+			isFirstMove: true,
 			numsSequence: [
-				[{value:3, isEmpty:false}, {value:2, isEmpty:false}, {value:1, isEmpty:false}],
-				[{value:7, isEmpty:false}, {value:4, isEmpty:false}, {value:8, isEmpty:false}],
-				[{value:5, isEmpty:false}, {value:6, isEmpty:false}, {value:'', isEmpty:true}]]
+				[{value:3, isEmpty:false}, {value:6, isEmpty:false}, {value:1, isEmpty:false}],
+				[{value:8, isEmpty:false}, {value:7, isEmpty:false}, {value:2, isEmpty:false}],
+				[{value:5, isEmpty:false}, {value:4, isEmpty:false}, {value:'', isEmpty:true}]]
 		}
 	},
 	methods:{
+		
+
 		changePosition(event){
 			let xClicked, yClicked, 
 					xEmpty, yEmpty
+
 
 			this.numsSequence.map((row, index) => {
 				//find x and y of clicked num
@@ -69,8 +72,50 @@ export default {
 				clickedCell.value = ''
 				clickedCell.isEmpty = true
 				}
-
 			}
+
+			if(!this.isFirstMove) this.isWinHandler()
+
+			this.isFirstMove = !this.isFirstMove
+		},
+
+		isWinHandler(){
+			const arrayOfCurrentSequence = []
+
+			this.numsSequence.map(row => row.map(num => arrayOfCurrentSequence.push(num.value)))
+
+			const rightSequence = new Array(arrayOfCurrentSequence.filter(val => val != '').sort((a, b) => a - b))
+
+			if(arrayOfCurrentSequence.filter(val => val != '').join() == rightSequence.join()){
+				this.isWin = true
+			}
+		},
+
+		shuffle(){
+			let arr = [1, 2, 3, 4, 5, 6, 7, 8],
+					shuffledSequence = []
+			
+			for(let i = 0; i < arr.length + 1; i++){
+				const randomIndex = Math.floor(Math.random() * arr.length)
+
+				shuffledSequence.push(arr[randomIndex])
+
+				arr.splice(randomIndex, 1)
+
+				i = 0
+			}
+
+		let iter = 0
+
+		this.numsSequence.map((row) => row.map((num) => {
+			if(num.value != ''){
+				num.value = shuffledSequence[iter]
+				num.isEmpty = false
+				iter++
+			}
+		}))
+
+		this.isWin = false
 		}
 	}
 }
