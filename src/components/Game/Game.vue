097be752efcdd5e9e53ad4b01a00d6b5878a129f
borwi4ok <donnos98@gmail.com>
2021-02-24 19:46:08 +0300

    ready game

--- src/components/Game/Game.vue
+++ src/components/Game/Game.vue
@@ -1,13 +1,20 @@
 <template>
 	<div class="gameContainer">
-			<template v-for="(row, index) in numsSequence">
+		<p v-if="isWin" class="isWinText">
+			Congratulations! You resolve this brainteaser
+		</p>
+
+			<template v-for="row in numsSequence">
 				<div 
 						v-for="el in row" 
-						:key="el" 
-						v-bind:class="[el.isEmpty ? 'empty' : 'num']">
-					<p>{{show(el.value,index)}}</p>
+						:key="el + Math.random()" 
+						v-bind:class="[el.isEmpty ? 'empty' : 'num']"
+						v-on:click="changePosition">
+					<p>{{el.value}}</p>
 				</div>
 			</template>
+
+			<button v-if="isWin" class="isWinBtn">Shuffle</button>
 	</div>
 </template>
 
@@ -19,15 +26,51 @@ export default {
 	name:'Game',
 	data(){
 		return{
+			isWin: true,
 			numsSequence: [
-				[{value:1, isEmpty:false}, {value:2, isEmpty:false}, {value:3, isEmpty:false}],
-				[{value:4, isEmpty:false}, {value:5, isEmpty:false}, {value:6, isEmpty:false}],
-				[{value:7, isEmpty:false}, {value:8, isEmpty:false}, {value:'', isEmpty:true}]]
+				[{value:3, isEmpty:false}, {value:2, isEmpty:false}, {value:1, isEmpty:false}],
+				[{value:7, isEmpty:false}, {value:4, isEmpty:false}, {value:8, isEmpty:false}],
+				[{value:5, isEmpty:false}, {value:6, isEmpty:false}, {value:'', isEmpty:true}]]
 		}
 	},
 	methods:{
-		show(num){
-			return num
+		changePosition(event){
+			let xClicked, yClicked, 
+					xEmpty, yEmpty
+
+			this.numsSequence.map((row, index) => {
+				//find x and y of clicked num
+				if(row.findIndex(num => num.value == +event.target.outerText) != -1){
+					xClicked = index
+					yClicked = row.findIndex(num => num.value == +event.target.outerText)
+					console.log(xClicked, yClicked)
+				}
+
+				//find x and y of empty elem
+				if(row.findIndex(num => num.value == '') != -1){
+					xEmpty = index
+					yEmpty = row.findIndex(num => num.value == '')
+					console.log(xEmpty, yEmpty)
+				}
+			})
+
+			// if clicked num in available row and col
+			if(Math.abs(xEmpty - xClicked) < 2 && Math.abs(yEmpty - yClicked) < 2){
+
+				//and it shouldn't be in diagonal cell
+				if(Math.abs(xEmpty - xClicked) + Math.abs(yEmpty - yClicked) != 2){
+				const clickedCell = this.numsSequence[xClicked][yClicked]
+				const emptyCell = this.numsSequence[xEmpty][yEmpty]
+
+				//change empty and clicked cells
+				emptyCell.value = clickedCell.value
+				emptyCell.isEmpty = false
+				
+				clickedCell.value = ''
+				clickedCell.isEmpty = true
+				}
+
+			}
 		}
 	}
 }
